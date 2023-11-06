5571d41a3c7f8d73f5fb514274b3f66461fa721d
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-06 19:50:22 +0100

    Update Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+# Escribir un programa que guarde en una variable el diccionario 
+# {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por
+# una divisa y muestre su símbolo o un mensaje de aviso si la 
+# divisa no está en el diccionario.
+currencyDict = {'Euro' : '€', 'Dollar' : '$', 'Yen' : '¥'} 
+
+badge = input ('Introduzca una divisa: ').title()
+
+if badge in currencyDict :
+    print(currencyDict.items('Euro'))
