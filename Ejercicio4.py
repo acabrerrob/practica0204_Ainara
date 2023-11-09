42c7e48f58031c9c2bff18f5e1b16f9d382ba7b4
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-10 00:08:23 +0100

    Update Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,7 @@
+personalInfo = {}
+
+while True :
+    key = input('Introduzca el nombre del dato que desee guardar: ')
+    value = input('Introduzca el valor del dato introducido previamente: ')
+    personalInfo[key] = value
+    print(personalInfo)
